683a7bb3273e11fa907f29656b6953e9a3750756
Pedro Werkhaizer <42971669+pedrowerkhaizer@users.noreply.github.com>
2023-03-21 08:30:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed  from './components/ChatFeed';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine 
+        height="100vh"
+        projectID="012e39f3-2fa2-45b2-ba2f-ce7a50a3de7c"
+        userName="pedrowerkhaizer"
+        userSecret="reactjs"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+        />
+    )
+//50 min    
+//https://www.youtube.com/watch?v=jcOKU9f86XE
 }
 
 export default App;
